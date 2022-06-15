507217d11c69228e59139977aa9650723813c7ad
imagno <igor.magno@protonmail.com>
2022-06-15 15:14:24 -0300

    corrected company import

--- people_management/settings.py
+++ people_management/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.companies',
+    'apps.companies.apps.CompaniesConfig',
 ]
 
 MIDDLEWARE = [
