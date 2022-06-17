e1e6f5d0f4c9e67cf602078b6d574aaa047ab420
imagno <igor.magno@protonmail.com>
2022-06-17 16:22:46 -0300

    refactoring name field

--- apps/employees/models.py
+++ apps/employees/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Employee(models.Model):
-    nome = models.CharField(max_length=50, help_text='employee name')
+    name = models.CharField(max_length=50, help_text='employee name')
 
     def __str__(self):
-        return self.nome
+        return self.name
