8d327801676d3aa13a90dad53fc72931e79f324c
imagno <igor.magno@protonmail.com>
2022-06-21 11:06:04 -0300

    fix include urlpatterns path

--- people_management/urls.py
+++ people_management/urls.py
@@ -1,5 +1,8 @@
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('apps.core.urls')),
+    path('employees/', include('apps.employees.urls')),
+    path('admin/', admin.site.urls),
 ]
