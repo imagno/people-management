8c8aa521509a9fc132db6b861b57de4b04f0a22b
imagno <igor.magno@protonmail.com>
2022-06-21 09:36:40 -0300

    created overtime_registration app

--- apps/documents/models.py
+++ apps/documents/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+from apps.employees.models import Employee
+
 
 class Document(models.Model):
     register_id = models.IntegerField(null=True, help_text='register document number')
     description = models.CharField(max_length=150, help_text='text description document')
+    employee = models.ForeignKey(Employee, on_delete=models.PROTECT)
 
     def __int__(self):
         return self.register_id
