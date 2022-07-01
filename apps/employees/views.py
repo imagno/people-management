45106e3aa7dc96c95eeebbbdc9bd1024f8fd82b6
imagno <igor.magno@protonmail.com>
2022-07-01 14:12:12 -0300

    implemented employees filtered by company

--- apps/employees/views.py
+++ apps/employees/views.py
@@ -6,3 +6,9 @@ from .models import Employee
 class ListEmployees(ListView):
     model = Employee
     paginated_by = 12
+
+    def get_queryset(self):
+        logged_company = self.request.user.employee.company
+        queryset = Employee.objects.filter(company=logged_company)
+
+        return queryset
